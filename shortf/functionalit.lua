a5c5af3e2706cb638da80944abd96526c97589af
MaciejSzreder <MSzreder3.14@wp.pl>
2023-02-11 15:09:23 +0100

    powering it

--- shortf/functionalit.lua
+++ shortf/functionalit.lua
@@ -119,6 +119,27 @@ function expression:__mod(other)
 	end)
 end
 
+function expression:__pow(other)
+	if is_it_function(self) and is_it_function(other) then
+		return register(function (arg)
+			return self(arg) ^ other(arg)
+		end)
+	end
+	if is_it_function(self) then
+		return register(function (arg)
+			return self(arg) ^ other
+		end)
+	end
+	if is_it_function(other) then
+		return register(function (arg)
+			return self ^ other(arg)
+		end)
+	end
+	return register(function (arg)
+		return self ^ other
+	end)
+end
+
 function expression:__concat(other)
 	if is_it_function(self) and is_it_function(other) then
 		return register(function (arg)
