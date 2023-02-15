a447ce89623a2d2b4a3988200d21da1e3181de6c
MaciejSzreder <MSzreder3.14@wp.pl>
2023-02-15 13:28:55 +0100

    merge callable it into common

--- shortf/it/common.lua
+++ shortf/it/common.lua
@@ -1,5 +1,8 @@
 local fun_mt
 local creator
+local child_creator
+
+local pass = function(...)return...end
 
 local it_functions = {}
 local function register(fun)
@@ -12,12 +15,12 @@ local function is_it_function(fun)
 end
 
 local function function_creator(fun)
-	return fun or function(...)return...end
+	return fun or pass
 end
 
-local function semifunction_creator(fun)
+local function callable_creator(fun)
 	return setmetatable({
-		action=fun or function(...)return...end
+		action=fun or pass
 	},fun_mt)
 end
 
@@ -25,6 +28,7 @@ local function initialize(options)
 	local install = options.install
 	local functional = options.functional
 	fun_mt = {}
+	child_creator = function_creator
 	if functional then
 		local fun = function()end
 		creator = function_creator
@@ -42,20 +46,25 @@ local function initialize(options)
 			end
 		end
 		-- there is no agree to install it or there is no option to install it
-		creator = semifunction_creator
+		creator = callable_creator
 		return	-- creator creates callable, fun_mt is new metatable
 	end
-	creator = expression.new
+	creator = callable_creator
+	child_creator = callable_creator
 end
 
 local function create(fun)
+	return register(child_creator(fun))
+end
+
+local function create_it(fun)
 	return register(creator(fun))
 end
 
 local function args(n)
 	local ret = {}
 	for i =  1,n do
-		ret[i] = create(function(...)return select(i,...)end)
+		ret[i] = create_it(function(...)return select(i,...)end)
 	end
 	return unpack(ret)
 end
@@ -66,24 +75,24 @@ return function (install)
 	local old_add = fun_mt.__add
 	function fun_mt:__add(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) + other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) + other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self + other(...)
 			end)
 		end
 		if old_add and (type(self)=='function' or type(other)=='function') then
 			return old_add(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self + other
 		end)
 	end
@@ -91,24 +100,24 @@ return function (install)
 	local old_sub = fun_mt.__sub
 	function fun_mt:__sub(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) - other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) - other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self - other(...)
 			end)
 		end
 		if old_sub and (type(self)=='function' or type(other)=='function') then
 			return old_sub(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self - other
 		end)
 	end
@@ -116,24 +125,24 @@ return function (install)
 	local old_mul = fun_mt.__mul
 	function fun_mt:__mul(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) * other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) * other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self * other(...)
 			end)
 		end
 		if old_mul and (type(self)=='function' or type(other)=='function') then
 			return old_mul(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self * other
 		end)
 	end
@@ -141,24 +150,24 @@ return function (install)
 	local old_div = fun_mt.__div
 	function fun_mt:__div(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) / other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) / other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self / other(...)
 			end)
 		end
 		if old_div and (type(self)=='function' or type(other)=='function') then
 			return old_div(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self / other
 		end)
 	end
@@ -166,24 +175,24 @@ return function (install)
 	local old_mod = fun_mt.__mod
 	function fun_mt:__mod(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) % other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) % other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self % other(...)
 			end)
 		end
 		if old_mod and (type(self)=='function' or type(other)=='function') then
 			return old_mod(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self % other
 		end)
 	end
@@ -191,24 +200,24 @@ return function (install)
 	local old_pow = fun_mt.__pow
 	function fun_mt:__pow(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) ^ other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) ^ other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self ^ other(...)
 			end)
 		end
 		if old_pow and (type(self)=='function' or type(other)=='function') then
 			return old_pow(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self ^ other
 		end)
 	end
@@ -216,24 +225,24 @@ return function (install)
 	local old_concat = fun_mt.__concat
 	function fun_mt:__concat(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) .. other(...)
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...) .. other
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self .. other(...)
 			end)
 		end
 		if old_concat and (type(self)=='function' or type(other)=='function') then
 			return old_concat(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self .. other
 		end)
 	end
@@ -241,24 +250,24 @@ return function (install)
 	local old_index = fun_mt.__index
 	function fun_mt:__index(other)
 		if is_it_function(self) and is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self(...)[other(...)]
 			end)
 		end
 		if is_it_function(self) then
-			return register(function (...)
+			return create(function (...)
 				return self(...)[other]
 			end)
 		end
 		if is_it_function(other) then
-			return register(function (...)
+			return create(function (...)
 				return self[other(...)]
 			end)
 		end
 		if old_index and type(self)=='function' then
 			return old_index(self,other)
 		end
-		return register(function (...)
+		return create(function (...)
 			return self[other]
 		end)
 	end
@@ -267,5 +276,5 @@ return function (install)
 		return self.action(...)
 	end
 
-	return create(), args
+	return create_it(), args
 end
